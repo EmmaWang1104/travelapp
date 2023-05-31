54270178a2d07d308a6f370ca218e5f6602ca3d6
Emma.W <emma.w@EmmaWdeMacBook-Pro.local>
2023-05-30 17:33:08 -0700

    Remove console

--- src/components/Title/index.js
+++ src/components/Title/index.js
@@ -4,7 +4,6 @@ import styles from './styles';
 
 function Title() {
     const [stateText, setText] = useState('');
-    console.log('UPDATED');
 
     function onTextPress(){
         setText('Updated state');
